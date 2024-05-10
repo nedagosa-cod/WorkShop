3659544162ee8878bbc0339c8d83e8793efb597e
nedagosa-cod <nestorcode4004@gmail.com>
2024-05-10 00:16:33 -0500

    icons agile

--- src/components/State1/Data.jsx
+++ src/components/State1/Data.jsx
@@ -25,7 +25,29 @@ const Data = ({ data }) => {
               <h2>Agile Training</h2>
             </div>
             <div className="card">
-              <div className="img"></div>
+              <div className="img">
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M12.0476 13.5826L4.5 17.9402C3.83333 18.3251 3 17.844 3 17.0742V8.00152C3 7.23171 3.83333 6.75059 4.5 7.13549L12.0476 11.4931V8.00151C12.0476 7.23171 12.881 6.75058 13.5476 7.13548L21.4048 11.6718C22.0714 12.0567 22.0714 13.019 21.4048 13.4039L13.5476 17.9402C12.881 18.3251 12.0476 17.844 12.0476 17.0742V13.5826Z"
+                      fill="#000000"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              </div>
               <div className="textBox">
                 <div className="textContent">
                   <p className="h1">BUSCAMOS INMEDIATEZ</p>
@@ -38,7 +60,26 @@ const Data = ({ data }) => {
               </div>
             </div>
             <div className="card">
-              <div className="img"></div>
+              <div className="img">
+                <svg
+                  fill="#000000"
+                  viewBox="0 0 14 14"
+                  role="img"
+                  focusable="false"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path d="m 11.8,10.6 0,-7.2035638 C 11.8,2.7367083 11.264337,2.2 10.603564,2.2 l -7.2071278,0 C 2.7367083,2.2 2.2,2.7356627 2.2,3.3964362 L 2.2,10.6 1,10.6 1,11.2 c 0,0.333681 0.2664272,0.6 0.5950819,0.6 l 10.8098361,0 C 12.726816,11.8 13,11.531371 13,11.2 l 0,-0.6 -1.2,0 z m -8.4,-7.2 7.2,0 0,5.4 -7.2,0 0,-5.4 z m 2.4,6.6 2.4,0 0,0.6 -2.4,0 0,-0.6 z"></path>
+                  </g>
+                </svg>
+              </div>
               <div className="textBox">
                 <div className="textContent">
                   <p className="h1">TECNOLOGÍA COBRA PROTAGONISMO</p>
@@ -51,7 +92,24 @@ const Data = ({ data }) => {
               </div>
             </div>
             <div className="card">
-              <div className="img"></div>
+              <div className="img">
+                <svg
+                  fill="#000000"
+                  viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path d="M13,0,10,3h2.3v9h1.4V3H16ZM5,5.7h6V4.3H5Zm0,3h6V7.3H5Zm0,3h6V10.3H5ZM3.7,4H2.3v9H0l3,3,3-3H3.7Z"></path>{" "}
+                  </g>
+                </svg>
+              </div>
               <div className="textBox">
                 <div className="textContent">
                   <p className="h1">ATENTO SE ADAPTA AL CAMBIO</p>
@@ -65,7 +123,26 @@ const Data = ({ data }) => {
               </div>
             </div>
             <div className="card">
-              <div className="img"></div>
+              <div className="img">
+                <svg
+                  fill="#000000"
+                  viewBox="-4 0 32 32"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <title>invert</title>{" "}
+                    <path d="M0 6.656h24v18.688h-24v-18.688zM12 23.75v-2.219l5.531-5.531-5.531-5.531v-2.219h-10.406v15.5h10.406zM12 21.531l-5.531-5.531 5.531-5.531v11.063z"></path>{" "}
+                  </g>
+                </svg>
+              </div>
               <div className="textBox">
                 <div className="textContent">
                   <p className="h1">NOS BASAMOS EN AULA INVERTIDA</p>
